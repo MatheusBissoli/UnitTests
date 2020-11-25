0c13074d5698f1e7f7d071f1f23ab05ca5fe42b9
Matheus Bissoli <matheus.bissoli@usemobile.xyz>
2020-11-25 15:13:19 -0300

    #add mockito dependencies added, test file for viewmodel added

---
+++ app/src/test/java/com/bissoli/unittest/OperationsViewModelTest.kt
@@ -0,0 +1,14 @@
+package com.bissoli.unittest
+
+import org.junit.Test
+
+class OperationsViewModelTest{
+
+    private lateinit var operationsViewModel: OperationsViewModel
+    private lateinit var operations: Operations
+
+    @Test
+    fun calcSum_numGiven_updateLiveData(){
+
+    }
+}
