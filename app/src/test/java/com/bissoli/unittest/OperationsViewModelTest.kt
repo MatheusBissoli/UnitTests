c14d23bb63ff9a0b08ea2ee732bf524a801faf93
Matheus Bissoli <matheus.bissoli@usemobile.xyz>
2020-11-25 15:45:55 -0300

    #add test for viewmodel added

--- app/src/test/java/com/bissoli/unittest/OperationsViewModelTest.kt
+++ app/src/test/java/com/bissoli/unittest/OperationsViewModelTest.kt
@@ -1,14 +1,31 @@
 package com.bissoli.unittest
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.google.common.truth.Truth.assertThat
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.mockito.Mockito
 
-class OperationsViewModelTest{
+class OperationsViewModelTest {
 
     private lateinit var operationsViewModel: OperationsViewModel
     private lateinit var operations: Operations
 
-    @Test
-    fun calcSum_numGiven_updateLiveData(){
+    @get:Rule
+    var instantTaskExecutorRole = InstantTaskExecutorRule()
 
+    @Before
+    fun setUp() {
+        operations = Mockito.mock(Operations::class.java)
+        Mockito.`when`(operations.sumNum(12, 12)).thenReturn(24)
+        operationsViewModel = OperationsViewModel(operations)
+    }
+
+    @Test
+    fun calcSum_numGiven_updateLiveData() {
+        operationsViewModel.calcSum(12, 12)
+        val result = operationsViewModel.sum.value
+        assertThat(result).isEqualTo(24)
     }
 }
