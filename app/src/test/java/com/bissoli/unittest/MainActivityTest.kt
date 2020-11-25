19b5b22d96ee44715d9b8ab9c92378df1234a95f
Matheus Bissoli <matheus.bissoli@usemobile.xyz>
2020-11-25 14:33:44 -0300

    #add sum tests added in MainActivityTest.kt

--- app/src/test/java/com/bissoli/unittest/MainActivityTest.kt
+++ app/src/test/java/com/bissoli/unittest/MainActivityTest.kt
@@ -1,18 +1,28 @@
 package com.bissoli.unittest
 
+import com.google.common.truth.Truth.assertThat
 import org.junit.Before
 import org.junit.Test
 
-import org.junit.Assert.*
-
 class MainActivityTest {
+
+    private lateinit var mainActivity: MainActivity
 
     @Before
     fun setUp() {
+        mainActivity = MainActivity()
     }
 
     @Test
-    fun sumNum() {
+    fun sumNum_numGiven_returnsSuccess() {
+        val result = mainActivity.sumNum(12, 12)
+        assertThat(result).isEqualTo(24)
+    }
+
+    @Test
+    fun sumNum_numNotGiven_returnsSuccess() {
+        val result = mainActivity.sumNum(0, 0)
+        assertThat(result).isEqualTo(0)
     }
 
     @Test
